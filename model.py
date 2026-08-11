dab5b666eeb10a856e28761aa13baf34c2844423
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-12 00:03:55 +0530

    Step 65: cross_entropy_loss

--- model.py
+++ model.py
@@ -434,8 +434,14 @@ def gather_correct_token_probs(probs, targets):
     """Return probs[i, targets[i]] for each i, shape (B,)."""
     return probs[np.arange(len(targets)), targets]
 
-# Step 65 - cross_entropy_loss (not yet solved)
-# TODO: implement
+# Step 65 - cross_entropy_loss
+import numpy as np
+
+def cross_entropy_loss(probs, targets):
+    """Mean negative log-likelihood over a batch."""
+    correct_probs = gather_correct_token_probs(probs, targets)
+    arr_log = array_log(correct_probs)
+    return -np.mean(arr_log)
 
 # Step 66 - derive_dlogits_on_paper (not yet solved)
 # TODO: implement
