85b5abc0a11b40095124932fa42f97addf44b49d
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-13 14:46:26 +0530

    Step 128: merge_heads_to_d_model

--- model.py
+++ model.py
@@ -1050,8 +1050,17 @@ def get_multihead_output_sequence_length(x_heads_back):
     """Return T from a (B, T, n_heads, d_head) tensor."""
     return x_heads_back.shape[1]
 
-# Step 128 - merge_heads_to_d_model (not yet solved)
-# TODO: implement
+# Step 128 - merge_heads_to_d_model
+import numpy as np
+
+def merge_heads_to_d_model(x_heads_back):
+    """Reshape (B, T, n_heads, d_head) into (B, T, d_model)."""
+    B = x_heads_back.shape[0]
+    T = x_heads_back.shape[1]
+    n_heads = x_heads_back.shape[2]
+    d_head = x_heads_back.shape[3]
+    d_model = n_heads * d_head
+    return x_heads_back.reshape(B, T, d_model)
 
 # Step 129 - multihead_output_projection_forward (not yet solved)
 # TODO: implement
