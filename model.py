0175ae184596dc844f2a45fe7a6d05c0dcf4c61e
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-13 14:44:40 +0530

    Step 127: get_multihead_output_sequence_length

--- model.py
+++ model.py
@@ -1045,8 +1045,10 @@ def multihead_weighted_sum(weights, v_heads):
 def transpose_heads_to_back(x_heads):
     return np.transpose(x_heads, (0, 2, 1, 3))
 
-# Step 127 - get_multihead_output_sequence_length (not yet solved)
-# TODO: implement
+# Step 127 - get_multihead_output_sequence_length
+def get_multihead_output_sequence_length(x_heads_back):
+    """Return T from a (B, T, n_heads, d_head) tensor."""
+    return x_heads_back.shape[1]
 
 # Step 128 - merge_heads_to_d_model (not yet solved)
 # TODO: implement
