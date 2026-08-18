724cd8382b701bce6bd4c7dff5b3df81d3be6805
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-18 11:20:43 +0530

    Step 159: take_last_position_logits

--- model.py
+++ model.py
@@ -1736,8 +1736,10 @@ def forward_to_get_logits(params, context_ids):
     logits, _ = full_model_forward(context_ids, params)
     return logits
 
-# Step 159 - take_last_position_logits (not yet solved)
-# TODO: implement
+# Step 159 - take_last_position_logits
+def take_last_position_logits(logits):
+    """Return logits at the final time step with shape (1, vocab_size)."""
+    return logits[:, -1, :]
 
 # Step 160 - apply_temperature (not yet solved)
 # TODO: implement
