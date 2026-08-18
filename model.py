cedb495d36d932dff07a8e04271c0b28cf1744b1
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-18 11:27:03 +0530

    Step 164: append_token_to_sequence

--- model.py
+++ model.py
@@ -1765,8 +1765,10 @@ def sample_one_token(probs, rng):
     """Sample one token id from probs of shape (1, vocab_size) using rng."""
     return int(rng.choice(probs.shape[1], p = probs[0]))
 
-# Step 164 - append_token_to_sequence (not yet solved)
-# TODO: implement
+# Step 164 - append_token_to_sequence
+def append_token_to_sequence(context_ids, token_id):
+    """Append token_id as a new final column to context_ids of shape (1, T)."""
+    return np.concatenate([context_ids, np.array([[token_id]], dtype = context_ids.dtype)], axis = 1)
 
 # Step 165 - generation_loop_for_n_steps (not yet solved)
 # TODO: implement
