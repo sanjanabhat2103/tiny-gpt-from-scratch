eeca39ab4d457046e527058f2dcc5c8e09df5745
Sanjana Bhat <sanjanabhat2103@gmail.com>
2026-08-18 10:25:32 +0530

    Step 148: initialize_adam_step_counter

--- model.py
+++ model.py
@@ -1537,8 +1537,10 @@ def initialize_adam_moments(model_params):
     v = zeros_like_tree(model_params)
     return m, v
 
-# Step 148 - initialize_adam_step_counter (not yet solved)
-# TODO: implement
+# Step 148 - initialize_adam_step_counter
+def initialize_adam_step_counter():
+    """Return the initial Adam step counter t."""
+    return 0
 
 # Step 149 - adam_increment_step (not yet solved)
 # TODO: implement
